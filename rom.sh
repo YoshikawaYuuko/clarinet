2477766cf6b9c48d744257c8b06a8180b1308480
Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>
2026-08-22 13:23:48 +0700

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # init & sync
-repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b seventeen --git-lfs --depth=1
-/opt/crave/resync.sh
+# repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b seventeen --git-lfs --depth=1
+# /opt/crave/resync.sh
 
 # device source
+rm -rf device/xiaomi/earth device/mediatek/sepolicy_vndr
 git clone https://github.com/YoshikawaYuuko/android_device_xiaomi_earth.git -b PixelOS-17 device/xiaomi/earth
 
 # Setup build
@@ -16,6 +17,7 @@ export BUILD_HOSTNAME=crave
 
 # start build
 breakfast earth userdebug
+make installclean
 m pixelos
 
 # Upload
