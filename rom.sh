0ba515ba706825b32c482992ad81b91319c091d3
Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>
2026-08-23 16:09:53 +0700

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # init & sync
-repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b seventeen --git-lfs --depth=1
-/opt/crave/resync.sh
+# repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b seventeen --git-lfs --depth=1
+# /opt/crave/resync.sh
 
 # device source
 # rm -rf device/xiaomi/earth device/mediatek/sepolicy_vndr
-git clone https://github.com/YoshikawaYuuko/android_device_xiaomi_earth.git -b PixelOS-17 device/xiaomi/earth
+# git clone https://github.com/YoshikawaYuuko/android_device_xiaomi_earth.git -b PixelOS-17 device/xiaomi/earth
 
 # Setup build
 . build/envsetup.sh
