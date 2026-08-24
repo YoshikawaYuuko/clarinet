8a1eaf0260642013ceec313aaf6e5665fd36324b
Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>
2026-08-24 11:20:12 +0700

    Comment out repo init and cloning commands in rom.sh
    Comment out repo initialization and cloning commands for build setup.

--- rom.sh
+++ rom.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
 # init & sync
-repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b seventeen --git-lfs --depth=1
-/opt/crave/resync.sh
+# repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b seventeen --git-lfs --depth=1
+# /opt/crave/resync.sh
 
 # device source
+rm -rf kernel/xiaomi/earth
 git clone https://github.com/YoshikawaYuuko/android_device_xiaomi_earth.git -b PixelOS-17 device/xiaomi/earth
 
 # Custom Source 
-rm -rf build/soong
-git clone https://github.com/YoshikawaYuuko/android_build_soong.git -b seventeen build/soong
+# rm -rf build/soong
+# git clone https://github.com/YoshikawaYuuko/android_build_soong.git -b seventeen build/soong
 
 # Setup build
 . build/envsetup.sh
