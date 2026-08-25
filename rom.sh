4ca32c09dff1c2118b561eeb5ed58f2a75358726
Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>
2026-08-25 10:28:49 +0700

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # init & sync
-# repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b seventeen --git-lfs --depth=1
-# /opt/crave/resync.sh
+repo init -u https://github.com/PixelOS-AOSP/android_manifest.git -b seventeen --git-lfs --depth=1
+/opt/crave/resync.sh
 
 # device source
-rm -rf device/xiaomi/earth
 git clone https://github.com/YoshikawaYuuko/android_device_xiaomi_earth.git -b PixelOS-17 device/xiaomi/earth
 
 # Setup build
