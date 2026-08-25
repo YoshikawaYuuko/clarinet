877645e9c3b277e2584fc6455d813984c4757f61
Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>
2026-08-25 10:28:28 +0700

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -12,13 +12,11 @@ git clone https://github.com/YoshikawaYuuko/android_device_xiaomi_earth.git -b P
 . build/envsetup.sh
 
 export SOONG_NINJA=ninja
-export GOGC=15
 export BUILD_USERNAME=yuuko
 export BUILD_HOSTNAME=crave
 
 # start build
-breakfast earth userdebug 
-make installclean 
+breakfast earth userdebug  
 m pixelos
 
 # Upload
