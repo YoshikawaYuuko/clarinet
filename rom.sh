160bae0f17556373deecc363128be0c42d5c51a2
Yuuko <317168157+YoshikawaYuuko@users.noreply.github.com>
2026-08-28 08:47:17 +0700

    Update rom.sh

--- rom.sh
+++ rom.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm -rf device/xiaomi/earth kernel/xiaomi/earth vendor/xiaomi/earth vendor/mediatek/ims
+rm -rf device/xiaomi/earth kernel/xiaomi/earth
 # rm -rf hardware/mediatek hardware/xiaomi device/mediatek/sepolicy_vndr
 
 # init & sync
