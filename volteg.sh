84a6b3e5b3e5eca90eee19061d1aa6b139e70175
Yuuko <oumaekumiko123@gmail.com>
2026-08-29 13:23:15 +0700

    Update volteg.sh

--- volteg.sh
+++ volteg.sh
@@ -5,22 +5,13 @@ repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs --n
 /opt/crave/resync.sh
 
 # device source
-git clone https://github.com/dreamsolister26/android_device_xiaomi_earth.git -b lineage-22.2 device/xiaomi/earth
-git clone https://github.com/dreamsolister26/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
-git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.1 kernel/xiaomi/earth --depth=1
-git clone https://github.com/dreamsolister26/fortissimo.git -b keys-new vendor/lineage-priv/keys
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
-git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
-
-# Setup build
-. build/envsetup.sh
+git clone https://github.com/dreamsolister26/android_device_xiaomi_earth.git -b crDroid-15 device/xiaomi/earth
 
 export BUILD_USERNAME=yuuko
 export BUILD_HOSTNAME=crave
-export TARGET_ENABLE_BLUR=false
-export TARGET_DISABLE_MATLOG=true
 
+# Setup build
+. build/envsetup.sh
 brunch earth userdebug  
 
 # Upload
