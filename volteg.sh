94688e1109c74a32a37b50f786c4ea2924816667
Yuuko <oumaekumiko123@gmail.com>
2026-08-29 13:17:59 +0700

    hi

--- volteg.sh
+++ volteg.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
 # init & sync
-repo init -u https://github.com/VoltageOS/manifest.git -b 17 --git-lfs --depth=1
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs --no-clone-bundle --depth=1
 /opt/crave/resync.sh
 
 # device source
-git clone https://github.com/dreamsolister26/android_device_xiaomi_earth.git -b Voltage-17 device/xiaomi/earth
+git clone https://github.com/dreamsolister26/android_device_xiaomi_earth.git -b lineage-22.2 device/xiaomi/earth
+git clone https://github.com/dreamsolister26/proprietary_vendor_xiaomi_earth.git -b lineage-22.2 vendor/xiaomi/earth
+git clone https://github.com/LineageOS/android_kernel_xiaomi_earth.git -b lineage-22.1 kernel/xiaomi/earth --depth=1
+git clone https://github.com/dreamsolister26/fortissimo.git -b keys-new vendor/lineage-priv/keys
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-22.2 hardware/mediatek
+git clone https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-22.2 device/mediatek/sepolicy_vndr
 
-# Custom Build Soong
-rm -rf build/soong
-git clone https://github.com/dreamsolister26/build_soong_voltage.git -b 17 build/soong
-
-export BUILD_USERNAME=yuuko
-export BUILD_HOSTNAME=crave
 
 # Setup build
 . build/envsetup.sh
+
+export BUILD_USERNAME=yuuko
+export BUILD_HOSTNAME=crave
+export TARGET_ENABLE_BLUR=false
+export TARGET_DISABLE_MATLOG=true
+
 brunch earth userdebug  
 
 # Upload
